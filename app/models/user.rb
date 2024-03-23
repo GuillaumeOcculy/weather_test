306ac76977a7c6bda1af84d4a617c2f00dc2b1df
Guillaume Occuly <occuly.guillaume@gmail.com>
2024-03-23 14:32:27 +0100

    Add user model
    Use bcrypt gem for authentication
    email is downcased

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+end
