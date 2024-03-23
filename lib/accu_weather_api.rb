50f354a17facd8012a2a454c3a85d19221968107
Guillaume Occuly <occuly.guillaume@gmail.com>
2024-03-23 18:36:19 +0100

    Add Accu Weather API
    add httparty and dotenv-rails gems
    
    The API returns the location_key and the forecast between today and +4 days

---
+++ lib/accu_weather_api.rb
@@ -0,0 +1,17 @@
+# Docs: https://developer.accuweather.com/apis
+
+class AccuWeatherApi
+  include HTTParty
+  base_uri "http://dataservice.accuweather.com"
+  ACCU_WEATHER_API_KEY = ENV.fetch("ACCU_WEATHER_API_KEY")
+
+  def self.get_location_key(location)
+    response = get("/locations/v1/cities/search", query: {apikey: ACCU_WEATHER_API_KEY, q: location})
+    response.first["Key"]
+  end
+
+  def self.get_forecast_five_days(location_key)
+    response = get("/forecasts/v1/daily/5day/#{location_key}", query: {apikey: ACCU_WEATHER_API_KEY})
+    response["DailyForecasts"]
+  end
+end
