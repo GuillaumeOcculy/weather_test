92ffb08485e0c1fb1da3a5b2649703b780857642
Guillaume Occuly <occuly.guillaume@gmail.com>
2024-03-23 15:33:19 +0100

    Add sessions_controller
    User can sign in and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_user
+    redirect_to [:new, :session], notice: "You must be logged in to access this page" unless current_user
+  end
 end
