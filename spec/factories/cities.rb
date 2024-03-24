a26aed189c02c1266d7d0dbfef54f93ecd0cb341
Guillaume Occuly <occuly.guillaume@gmail.com>
2024-03-24 18:14:00 +0100

    Add City model spec

---
+++ spec/factories/cities.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :city do
+    name { "Paris" }
+    accu_weather_key { "123" }
+  end
+end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:accu_weather_key) }
+  end
+
+  describe 'callbacks' do
+    describe 'before_save' do
+      it 'should sanitize name' do
+        city = create(:city, name: 'New-York')
+        expect(city.name).to eq('new-york')
+      end
+    end
+  end
 end
