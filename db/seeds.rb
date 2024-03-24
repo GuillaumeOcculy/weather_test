0b39ec0badfd1edc9ef416ddcf1a4ea49284c1de
Guillaume Occuly <occuly.guillaume@gmail.com>
2024-03-24 22:02:41 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create(email: 'test@example.com', password: 'password', prefered_city: 'paris')
+city = City.create(name: 'Paris', accu_weather_key: '623')
+
+forecast = FactoryBot.create(:forecast, city: city)
+
+puts 'Data seeded'
+puts "user credentials: email: #{user.email}, password: password"
