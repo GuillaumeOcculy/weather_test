a63139cb193b962bdc095dabbce88af858562fe6
Guillaume Occuly <occuly.guillaume@gmail.com>
2024-03-24 01:28:49 +0100

    Create get_or_create_city_service.rb

---
+++ app/services/get_or_create_city_service.rb
@@ -0,0 +1,26 @@
+require "accu_weather_api"
+
+class GetOrCreateCityService < ApplicationService
+  validates :name, presence: true
+
+  attr_reader :name
+  attr_accessor :city
+
+  def initialize(name)
+    @name = name
+  end
+
+  def call
+    find_or_create_city
+  rescue StandardError => e
+    fail!(e.message)
+  end
+
+  private
+
+  def find_or_create_city
+    self.city = City.find_or_create_by(name: name) do |city|
+      city.accu_weather_key = AccuWeatherApi.get_location_key(name)
+    end
+  end
+end
