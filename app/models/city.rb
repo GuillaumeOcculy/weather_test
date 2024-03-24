01e57cd52603fe6da7b8e51a302aecad96eb0bf5
Guillaume Occuly <occuly.guillaume@gmail.com>
2024-03-24 14:47:53 +0100

    Add validation for city names

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,7 @@
 class City < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  NAMES = ["London", "New-York", "Paris", "Seoul", "Tokyo"].freeze
+
+  validates :name, presence: true, uniqueness: true, inclusion: { in: NAMES }
   validates :accu_weather_key, presence: true
 
   before_save :sanitize_name
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true
+  validates :prefered_city, inclusion: { in: City::NAMES }
 
+  before_validation :sanitize_prefered_city
   before_save :downcase_email
 
   private
+
+  def sanitize_name
+    self.name = name.strip.downcase
+  end
 
   def downcase_email
     self.email = email.downcase
